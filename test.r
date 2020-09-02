d43ad87fcb922cdbdfc236ab16121efb37cb23e8
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2020-09-02 16:03:02 +0530

    Update test.r

--- test.r
+++ test.r
@@ -3,3 +3,12 @@ new.function <- function() {
       print(i^2)
    }
 }	
+
+fun = function(one){
+   one.plus.one <- matrix(1:10, nrow = 2)
+   four[1,]
+   txt <- 'hi'
+   three <- two+ 1
+   if(txt == 'hi')4
+   5}
+}
