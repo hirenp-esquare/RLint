c1c5f1b391618fa6ed49c89d7638935edcd4b7d5
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2020-09-29 16:22:00 +0530

    Add files via upload

--- R/hello.R
+++ R/hello.R
@@ -1,13 +1,9 @@
+add <- function(x) x + 1
 
-
-add <- function(x){
-
-  if (x == 1) {
-    x + 1
-  } else{
-    x + 5
+add_even <- function(x, y){
+  if (y %% 2 == 0) {
+    x + y;
+  }else{
+    x;
   }
-
 }
-
-
