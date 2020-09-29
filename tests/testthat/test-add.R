4ae5cdceb22d5cde31f66ee0b16b71ec18603dfb
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2020-09-29 16:20:24 +0530

    Add files via upload

--- tests/testthat/test-add.R
+++ tests/testthat/test-add.R
@@ -1,7 +1,8 @@
- test_that("add works", {
-  expect_equal(add(1), 2)
-  #expect_equal(adds(1), 2)
-  #expect_equal(addthird(1), 2)
-  #expect_equal(add(2), 7)
-})
 
+
+test_that("add works", {
+  expect_equal(add(1), 2)
+  expect_equal(add(2), 3)
+
+  expect_equal(add_even(1, 2), 3)
+})
