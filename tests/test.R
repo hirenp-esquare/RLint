5ee8c5051e4160aeb820b22e0b09129703d87604
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2020-09-29 10:42:12 +0530

    Update test.R

--- tests/test.R
+++ tests/test.R
@@ -1,3 +1,9 @@
-stopifnot(testpackage:::add_one(1) == 2)
+test_that("add works", {
+  expect_equal(add(1), 2)
+  #expect_equal(adds(1), 2)
+  #expect_equal(addthird(1), 2)
+  #expect_equal(add(2), 7)
+})
+
 
  
